e950d59cf0a75328f7c5494798a1b32943522cb3
NonRoute <chanon.ratn@gmail.com>
2023-10-16 17:36:25 +0700

    Make login page use next-auth

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,15 +1,41 @@
+'use client'
 import Center from '@/components/Center'
 import { TextField } from '@mui/material'
+import { signIn } from 'next-auth/react'
 import Link from 'next/link'
+import { useRef } from 'react'
 
 export default function Login() {
+	const email = useRef('')
+	const password = useRef('')
+
 	return (
 		<div>
 			<Center header="Login">
-				<TextField label="Email" type="email" variant="filled" />
-				<TextField label="Password" type="password" variant="filled" />
+				<TextField
+					label="Email"
+					type="email"
+					variant="filled"
+					onChange={(e) => (email.current = e.target.value)}
+				/>
+				<TextField
+					label="Password"
+					type="password"
+					variant="filled"
+					onChange={(e) => (password.current = e.target.value)}
+				/>
 				<div className="flex flex-col gap-1">
-					<button className="bg-gradient-to-br from-cyan-500 to-sky-600 hover:from-cyan-400 hover:to-sky-500 py-2 px-3 text-white font-semibold rounded-md hover:bg-slate-200 text-lg">
+					<button
+						className="bg-gradient-to-br from-cyan-500 to-sky-600 hover:from-cyan-400 hover:to-sky-500 py-2 px-3 text-white font-semibold rounded-md hover:bg-slate-200 text-lg"
+						onClick={async () =>
+							await signIn('credentials', {
+								email: email.current,
+								password: password.current,
+								redirect: true,
+								callbackUrl: '/'
+							})
+						}
+					>
 						Login
 					</button>
 					<Link href="/register" className="text-sky-600 font-semibold underline ml-auto">
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,7 +1,12 @@
+'use client'
+import { signIn, signOut, useSession } from 'next-auth/react'
 import Link from 'next/link'
 import { PiBagFill } from 'react-icons/pi'
+import { BiSolidUser } from 'react-icons/bi'
 
 export default function Navbar() {
+	const { data: session, status } = useSession()
+
 	return (
 		<div className="bg-white h-14 text-black flex items-center justify-between p-4 shadow">
 			<div className="flex items-center gap-3">
@@ -14,18 +19,27 @@ export default function Navbar() {
 					<Link href="/create">Create company</Link>
 				</div>
 			</div>
-			<Link
-				href="/login"
-				className="border-sky-500 border-2 py-1 px-3 text-sky-600 font-semibold rounded-md hover:bg-slate-200"
-			>
-				Login
-			</Link>
-			{/* <Link
-				href="/logout"
-				className="border-sky-600 border-2 py-1 px-3 text-sky-600 font-semibold rounded-md hover:bg-slate-200"
-			>
-				Logout
-			</Link> */}
+			{session ? (
+				<div className="flex gap-4">
+					<div className="flex gap-2 items-center">
+						<BiSolidUser />
+						{session?.user?.name}
+					</div>
+					<button
+						onClick={() => signOut()}
+						className="border-sky-600 border-2 py-1 px-3 text-sky-600 font-semibold rounded-md hover:bg-slate-200"
+					>
+						Logout
+					</button>
+				</div>
+			) : (
+				<button
+					onClick={() => signIn()}
+					className="border-sky-500 border-2 py-1 px-3 text-sky-600 font-semibold rounded-md hover:bg-slate-200"
+				>
+					Login
+				</button>
+			)}
 		</div>
 	)
 }
