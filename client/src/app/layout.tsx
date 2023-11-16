6a171feec8f6c91d490af811a2369d4def0170ba
watcharavit <watcha.ea@hotmail.com>
2023-11-16 11:08:08 +0700

    Update import path

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 import Navbar from '../components/Navbar'
 import { getServerSession } from 'next-auth'
 import { authOptions } from './api/auth/[...nextauth]/route'
-import NextAuthProvider from './providers/NextAuthProvider'
+import NextAuthProvider from '@/providers/NextAuthProvider'
 import { ToastContainer } from 'react-toastify'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -22,18 +22,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
 		<html lang="en">
 			<body className={inter.className}>
 				<NextAuthProvider session={nextAuthSession}>
-					<ToastContainer
-						position="top-center"
-						autoClose={2000}
-						hideProgressBar={false}
-						newestOnTop={false}
-						closeOnClick
-						rtl={false}
-						pauseOnFocusLoss
-						draggable
-						pauseOnHover
-						theme="light"
-					/>
+					<ToastContainer position="top-center" autoClose={2000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="light" />
 					<div className="bg-gradient-to-br from-cyan-500 to-sky-600 min-h-screen pb-0.5">
 						<Navbar />
 						{children}
