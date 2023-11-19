151dc12e7df0aeec4a380a7556857a6cfadd26c1
watcharavit <watcha.ea@hotmail.com>
2023-11-20 02:48:30 +0700

    Add booking time to product card

--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -2,6 +2,10 @@
 import { CompanyItem } from "@/interface/Interface";
 import Link from "next/link";
 import CompanyDetail from "./CompanyDetail";
+import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import getBooking from "@/libs/getBooking";
+import dayjs from "dayjs";
 
 export default function ProductCard({
 	company,
@@ -14,9 +18,33 @@ export default function ProductCard({
 	isAdmin?: boolean;
 	sessionId?: string;
 }) {
+	const { data: session, status } = useSession();
+	const [bookingTime, setBookingTime] = useState<string>("");
+	const fetchData = async () => {
+		if (session && sessionId) {
+			const bookingData = await getBooking(session.user.token, sessionId);
+			const bookingDate = dayjs().format("YYYY-MM-DD");
+			const bookingStart =
+				dayjs(bookingData.data.bookingStart).get("hour") +
+				":" +
+				dayjs(bookingData.data.bookingStart).get("minute");
+			const bookingEnd =
+				dayjs(bookingData.data.bookingEnd).get("hour") + ":" + dayjs(bookingData.data.bookingEnd).get("minute");
+			const time = bookingDate + " " + bookingStart + "-" + bookingEnd;
+			setBookingTime(time);
+		}
+	};
+
+	useEffect(() => {
+		if (session && session.user.token) {
+			fetchData();
+		}
+	}, [session]);
+
 	return (
 		<div className="bg-white rounded-lg shadow-md overflow-hidden py-4 px-6 gap-2 flex flex-col w-full justify-between h-full">
 			<CompanyDetail company={company} />
+			<div className="text-cyan-600 font-semibold text-l text-center">{bookingTime}</div>
 			<div className="gap-2 flex flex-col">
 				{isLogin ? (
 					sessionId ? (
