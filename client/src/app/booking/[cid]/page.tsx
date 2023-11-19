3e91a22bb5bd50b1dee7e85050d6d255d47d1701
watcharavit <watcha.ea@hotmail.com>
2023-11-19 22:37:00 +0700

    Change to client component

--- client/src/app/booking/[cid]/page.tsx
+++ client/src/app/booking/[cid]/page.tsx
@@ -1,30 +1,44 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-// import getUserProfile from "@/libs/getUserProfile";
+"use client";
 import CompanyDetail from "@/components/CompanyDetail";
 import getBookings from "@/libs/getBookings";
 import getCompany from "@/libs/getCompany";
 import { SessionList } from "@/components/SessionList";
+import getUserProfile from "@/libs/getUserProfile";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { CompanyItem } from "@/interface/Interface";
 
-export default async function Booking({ params }: { params: { cid: string } }) {
-	const session = await getServerSession(authOptions);
+export default function Booking({ params }: { params: { cid: string } }) {
+	const { data: session, status } = useSession();
+	const [profile, setProfile] = useState<any>();
+	const [company, setCompany] = useState<CompanyItem>();
+	const [sessionBookings, setSessionBookings] = useState<any>();
+
 	if (!session || !session.user.token) return null;
-	// const profile = await getUserProfile(session.user.token);
-	// if (profile.data.role != "admin") return <div>No permission</div>;
-	const company = (await getCompany(params.cid)).data;
-	const sessionBookings = (await getBookings(session.user.token, params.cid)).data;
+
+	const fetchData = async () => {
+		setProfile(await getUserProfile(session.user.token));
+		setCompany((await getCompany(params.cid)).data);
+		setSessionBookings((await getBookings(session.user.token, params.cid)).data);
+	};
+
+	useEffect(() => {
+		fetchData();
+	}, []);
 
 	return (
 		<div className="m-8">
 			<div className="text-5xl font-bold text-white my-4 pt-2">Interview Session</div>
 			<div className="p-8 gap-8 bg-white rounded-md shadow flex flex-col">
-				<CompanyDetail company={company} />
-				<SessionList
-					userToken={session.user.token}
-					userId={session.user._id}
-					sessions={sessionBookings}
-					mode="book"
-				/>
+				{company && <CompanyDetail company={company} />}
+				{profile && sessionBookings && (
+					<SessionList
+						userToken={session.user.token}
+						userId={profile.data._id}
+						sessions={sessionBookings}
+						mode="book"
+					/>
+				)}
 			</div>
 		</div>
 	);
